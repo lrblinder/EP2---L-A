767f41a6f4f112844d171a3786bb989e76b92872
Luisa Blinder <luisarb@al.insper.edu.br>
2023-04-26 17:47:05 -0300

    def defiine posições

--- jogo.py
+++ jogo.py
@@ -1,1 +1,12 @@
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    lista_saida=[]
+    
+    for i in range (tamanho):
+        posicao =[linha,coluna]
+        if orientacao == "vertical":
+            posicao[0] += i
+        if orientacao == "horizontal":
+            posicao[1] += i
+        lista_saida.append(posicao)    
+    return lista_saida     
 
